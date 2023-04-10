cd9913309326019829f5af20618282d2d2939836
Eric McElyea <ericmcelyea@gmail.com>
2023-04-10 14:25:31 -0700

    wrapup basic version

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ Testing a scraper for keyword mentions on reddit
 ### Considerations
 The ideal performance would be a user types in a keyword (i.e. spiderman) and a list of every occurence is returned.
 This is untenable for a few reasons:
-* Reddit API does not support this behavior natively, it is effective for recent searches over specific subreddits, but not historical searches.
-* Reddit generates 25+ billion comments a month, that is > 23 GB per month of text, an exhaustive search for all the years would be tricky.
 
-So we have two problems that we can solve in two different ways:
-* To check historical mentions we can pull all the comments as a zip from [reddit comment history](https://files.pushshift.io/reddit/comments/). This could be impractical but we can check the viability of storing petabytes of text and grepping it.
-* To check recent mentions we can leverage the reddit API
+### Conclusion
+* This was fairly successful, I decided to get the most value out of this we want two fields: An object and a list of verbs.
+* If you just search for an object i.e. 'carrots' you are going to get a ludicrous amount of noise around your search.
+* I included a list of verbs that we also check for in the comments. So I want all comments around ('carrots', ['cook', 'plant'])
 
+ ### TODO
+ * figure out some way around the rate-limiting so I can pull more data from comments (100 per request but would be nice to get a ~a thousand comments to parse), I could achieve this by rewriting this on the FE and have users make requests instead of the server, or creating several API users (it is pretty easy to make multiple reddit accounts).
+ * Move all the filtering and verb stuff to the frontend, this should just return the results, that was users can decide how to filter the data once they have it
 
--- scraper.py
+++ scraper.py
@@ -1,20 +1,97 @@
-import json
-import praw
 import os
+import requests
+import base64
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
+
+user_agent = 'web:product-scraper:v1 (by u/asdfasdfs)'
 client_id = os.getenv('REDDIT_CLIENT_ID')
 client_secret = os.getenv('REDDIT_CLIENT_SECRET')
-user_agent = 'web:product-scraper:v1 (by u/collectorius)'
-reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
 
-q='terrier' #query
-sub='dogs' #subreddit
-sort = "new" 
-limit = 1
+# Encode the client ID and secret in base64 format for authentication
+auth_string = base64.b64encode(f'{client_id}:{client_secret}'.encode()).decode()
+headers = {'User-Agent': user_agent, 'Authorization': f'Basic {auth_string}'}
 
-top_posts = reddit.subreddit(sub).search(q, sort=sort, limit=limit)
+# We will stop requesting comments when we get to 200
+MAX_RESULTS = 199
+def search_comments_by_keyword(keyword):
+    # Set up the API endpoint and parameters
+    url = 'https://www.reddit.com/search.json'
+    params = {'q': keyword, 'type': 'comment', 'limit': 100, 'sort': 'new'}
+    comments = []
+    while len(comments) < MAX_RESULTS:
+        # Send the request and get the response JSON data
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+        comments.extend(data['data']['children'])
+        # break if no more comments
+        if len(data["data"]["children"]) == 0:
+            break
+        # update after param to query comments prior to first one
+        if len(comments) < MAX_RESULTS:
+            last_comment_id = comments[-1]["data"]["name"]
+            params["after"] = last_comment_id
+        else:
+            break
+        time.sleep(0.1)  # Sleep for 100ms between each loop
+    return comments
 
-for post in top_posts:
-    print(vars(post))
+def extract_string(s, keyword, verb):
+    keyword_forms = generate_verb_forms(verb.lower())
+    for keyword in keyword_forms:
+        start_index = s.find(keyword)
+        end_index = s.find(keyword)
+        if start_index != -1 and end_index != -1:
+            if start_index > end_index:
+                start_index, end_index = end_index, start_index
+            return True
+    return False
+
+# This will generate all possible verb forms for a search term: i.e. 'trades'
+def generate_verb_forms(verb_root):
+    suffixes = ['s', 'ing', 'ed', 'en', 'er', 'est', 'able']
+    verb_forms = [verb_root]
+
+    for suffix in suffixes:
+        if suffix == 's':
+            if verb_root[-1] == 's' or verb_root[-1] == 'x' or verb_root[-1] == 'z' or (verb_root[-2:] == 'ch' or verb_root[-2:] == 'sh'):
+                verb_forms.append(verb_root + 'es')
+            else:
+                verb_forms.append(verb_root + suffix)
+        elif suffix == 'ing':
+            if verb_root[-1] == 'e':
+                verb_forms.append(verb_root[:-1] + suffix)
+            else:
+                verb_forms.append(verb_root + suffix)
+        elif suffix == 'ed':
+            if verb_root[-1] == 'e':
+                verb_forms.append(verb_root[:-1] + suffix)
+            else:
+                verb_forms.append(verb_root + suffix)
+        elif suffix == 'en':
+            if verb_root[-1] == 'e':
+                verb_forms.append(verb_root[:-1] + suffix)
+            else:
+                verb_forms.append(verb_root + suffix)
+        elif suffix == 'er' or suffix == 'est' or suffix == 'able':
+            verb_forms.append(verb_root + suffix)
+    return verb_forms
+
+def query_product(product, verbs):
+    comments = search_comments_by_keyword(product)
+    valid = []
+    for comment in comments:
+        for verb in verbs:
+            if extract_string(comment['data']['selftext'], product, verb):
+                valid.append(comment['data'])
+                break
+    
+    return valid
+
+
+hits = query_product('drum kits', ['sell','buy', 'trade'])
+for hit in hits:
+    print(hit['selftext'])
